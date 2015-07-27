9034d3a1f18be50d70b7ea79ccc1f543b1b458f1
Sergey <sbeznochenko@gmail.com>
2015-07-27 13:27:10 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Административный доступ</title>
+	
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="stylesheets/reset.css">
+	<link rel="stylesheet" href="stylesheets/style.css">
+
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+</head>
+<body>
+	<header>
+		<div class="header_top">
+			<div class="menu_header_left uni_display">
+				<button type="button" class="btn btn_navbar uni_display">
+						<span class="icon-bar">
+						</span>
+						<span class="icon-bar">
+						</span>
+						<span class="icon-bar">
+						</span>
+				</button>
+				<div class="logotype uni_display">
+					<a href="#">
+						company name
+					</a>
+				</div>
+			</div>
+			<div class="menu_header_rigth uni_display">
+				<div class="uni_display">
+					<a href="http://www.web.com" class="icon_button">
+						<i class="fa fa-external-link"></i>
+						Перейти на www.web.com
+					</a>
+				</div>
+				<div class="btn_header_admin uni_display">
+					<button type="button" class="btn admin_header_btn" data-toggle="dropdown" aria-expanded="false">
+						<i class="fa fa-user icon_marg_r"></i>
+						Привет, admin_name
+						<i class="fa fa-caret-down icon_marg_l"></i>
+					</button>
+					<ul class="dropdown-menu" role="menu">
+						<li>
+							
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
+</body>
+</html>
